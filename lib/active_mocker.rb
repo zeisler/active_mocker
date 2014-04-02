66d0abebaabbe19d49741158aa2f77a0bde1195d
Dustin Zeisler <dustin@zive.me>
2014-04-01 17:44:19 -0700

    load all file under main file

--- lib/active_mocker.rb
+++ lib/active_mocker.rb
@@ -1,5 +1,16 @@
 require "active_mocker/version"
-
+$:.unshift File.expand_path('../../', __FILE__)
+require 'string_reader'
+require 'active_mocker/table'
+require 'active_mocker/config'
+require 'active_mocker/reparameterize'
+require 'active_mocker/field'
+require 'active_mocker/active_record'
+require 'active_mocker/model_reader'
+require 'active_mocker/schema_reader'
+require 'active_mocker/active_record/schema'
+require 'active_mocker/base'
+require 'active_support/all'
 module ActiveMocker
-  # Your code goes here...
+
 end

48d38fb939dfd1d8a074bdfc2f9b50004c0a2c7c
Dustin Zeisler <dustin@zive.me>
2014-04-01 17:54:09 -0700

    add file_reader to require list

--- lib/active_mocker.rb
+++ lib/active_mocker.rb
@@ -1,16 +1,19 @@
 require "active_mocker/version"
 $:.unshift File.expand_path('../../', __FILE__)
-require 'string_reader'
+require 'active_support/all'
 require 'active_mocker/table'
+require 'active_mocker/field'
+require 'file_reader'
 require 'active_mocker/config'
 require 'active_mocker/reparameterize'
-require 'active_mocker/field'
 require 'active_mocker/active_record'
 require 'active_mocker/model_reader'
 require 'active_mocker/schema_reader'
 require 'active_mocker/active_record/schema'
 require 'active_mocker/base'
-require 'active_support/all'
+require 'active_mocker/active_record'
+require 'active_mocker/model_reader'
+require 'active_mocker/reparameterize'
 module ActiveMocker
 
 end

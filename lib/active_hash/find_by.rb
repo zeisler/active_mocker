f7d14d786584a05fdd86f47c28d13f411a8a5e4f
Dustin Zeisler <dustin@zive.me>
2014-04-18 16:41:21 -0700

    find_by! return ActiveRecord::RecordNotFound if not found

--- lib/active_hash/find_by.rb
+++ lib/active_hash/find_by.rb
@@ -1,11 +1,18 @@
 module ActiveHash
 
   module ARApi
+    require 'active_record/errors'
 
     module FindBy
 
       def find_by(options = {})
         send(:where, options).first
+      end
+
+      def find_by!(options={})
+        result = find_by(options)
+        raise ActiveRecord::RecordNotFound if result.blank?
+        result
       end
 
     end

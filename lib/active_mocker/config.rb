7e7395fddcd81d1130e288ff790af6ac41ff66d1
Dustin Zeisler <dustin@zive.me>
2014-04-18 16:37:24 -0700

    Deprecate methods: active_hash_as_base=, model_relationships=, model_methods=, mass_assignment=. They have been combined into ,schema_attributes= and model_attributes=.

--- lib/active_mocker/config.rb
+++ lib/active_mocker/config.rb
@@ -5,45 +5,57 @@ module ActiveMocker
 
     attr_accessor :schema_file,
                   :model_dir,
-                  :active_hash_as_base,
                   :schema_attributes,
-                  :model_relationships,
-                  :model_methods,
-                  :mass_assignment,
+                  :model_attributes,
                   :schema_file_reader,
-                  :model_file_reader
+                  :model_file_reader,
+                  :clear_cache
 
     def config
       @@first_load ||= reload_default
       yield self
       check_required_settings
+      require_active_hash
     end
 
     def reload_default
+      @log_level           = Logger::WARN
       @schema_file         = nil
       @model_dir           = nil
-      @active_hash_as_base = false
       @schema_attributes   = true
-      @model_relationships = true
-      @model_methods       = true
-      @mass_assignment     = true
-      @log_level           = Logger::WARN
+      @model_attributes    = true
+      @clear_cache         = false
+      @schema_file_reader  = nil
+      @model_file_reader   = nil
     end
 
     def check_required_settings
-      raise 'schema_file must be specified'if schema_file.nil?
-      raise 'model_dir must be specified'if model_dir.nil?
+      raise 'schema_file must be specified' if schema_file.nil?
+      raise 'model_dir must be specified'   if model_dir.nil?
+    end
+
+    def require_active_hash
+      require 'active_hash'  if schema_attributes
     end
 
     def log_level=(level)
       Logger_.level = level
     end
 
-    def active_hash_as_base=(bool)
-      if bool
-        require 'active_hash'
-      end
-      @active_hash_as_base = bool
+    def active_hash_as_base=(arg)
+      Logger_.warn('Deprecation Warning: config option `active_hash_as_base` is now model_attributes')
+    end
+
+    def model_relationships=(arg)
+      Logger_.warn('Deprecation Warning: config option `model_relationships` is now model_attributes')
+    end
+
+    def model_methods=(arg)
+      Logger_.warn('Deprecation Warning: config option `model_methods` is now model_attributes')
+    end
+
+    def mass_assignment=(arg)
+      Logger_.warn('Deprecation Warning: config option `mass_assignment` is now model_attributes')
     end
 
   end

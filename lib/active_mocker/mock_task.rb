80b09b93b6f7cd408daee905b5c0bae0c2c12891
Dustin Zeisler <dustin@zive.me>
2014-05-14 17:40:35 -0700

    deleting files

---
+++ lib/active_mocker/mock_task.rb
@@ -0,0 +1,12 @@
+# http://blog.nathanhumbert.com/2010/02/rails-3-loading-rake-tasks-from-gem.html
+
+# ['db:schema:load', 'db:migrate', 'db:reset'].each do |task|
+#   Rake::Task[task].enhance do
+#     Rake::Task['rebuild_mocks'].invoke
+#   end
+# end
+
+task rebuild_mocks: :environment do
+  puts 'rebuilding mocks'
+  ActiveMocker::Generate.new
+end

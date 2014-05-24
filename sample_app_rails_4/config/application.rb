8120975b5c3387c735c9c214e37ec01491ce0a24
Dustin Zeisler <dustin@zive.me>
2014-05-23 23:10:26 -0700

    possible performance improvement for running tests

--- sample_app_rails_4/config/application.rb
+++ sample_app_rails_4/config/application.rb
@@ -5,7 +5,7 @@
     # require "rails/test_unit/railtie"
 
     # Assets should be precompiled for production (so we don't need the gems loaded then)
-    Bundler.require(*Rails.groups(assets: %w(development test)))
+    # Bundler.require(*Rails.groups(assets: %w(development test)))
 
     module SampleApp
       class Application < Rails::Application

1fc36057a628168f830bc090f1015bf16b2a328c
Dustin Zeisler <dustin@zive.me>
2014-06-14 17:47:29 -0700

    refactor unknown module logging

--- lib/active_mocker/active_record/unknown_module.rb
+++ lib/active_mocker/active_record/unknown_module.rb
@@ -3,21 +3,20 @@ module ActiveMocker
   module UnknownModule
 
     def include(_module)
-      begin
-        super _module
-      rescue => e
-
-        Logger.debug "ActiveMocker :: Debug :: Can't include module #{_module} from class #{self.name}.\n\t\t\t\t\t\t\t\t#{caller}\n"
-        Logger.debug "\t\t\t\t\t\t\t\t#{e}"
-
-      end
+      try_and_log('include', _module)
     end
 
     def extend(_module)
+      try_and_log('extend', _module)
+    end
+
+    private
+
+    def try_and_log(type, name)
       begin
         super _module
       rescue => e
-        Logger.debug "ActiveMocker :: Debug :: Can't extend module #{_module} from class #{self.name}.\n\t\t\t\t\t\t\t\t#{caller}\n"
+        Logger.debug "ActiveMocker :: Debug :: Can't #{type} module #{name} from class #{self.name}.\n\t\t\t\t\t\t\t\t#{caller}\n"
         Logger.debug "\t\t\t\t\t\t\t\t#{e}"
       end
     end

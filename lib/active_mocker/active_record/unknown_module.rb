e1dcce554387ed1aeb5f4a2a861377d3f2d05c16
Dustin Zeisler <dustin@zive.me>
2014-06-25 12:59:44 -0700

    UnknownModule will have correct caller in log.

--- lib/active_mocker/active_record/unknown_module.rb
+++ lib/active_mocker/active_record/unknown_module.rb
@@ -3,20 +3,20 @@ module ActiveMocker
   module UnknownModule
 
     def include(_module)
-      try_and_log('include', _module)
+      try_and_log('include', _module, caller)
     end
 
     def extend(_module)
-      try_and_log('extend', _module)
+      try_and_log('extend', _module, caller)
     end
 
     private
 
-    def try_and_log(type, name)
+    def try_and_log(type, name, _caller)
       begin
         super _module
       rescue => e
-        Logger.debug "ActiveMocker :: Debug :: Can't #{type} module #{name} from class #{self.name}.\n\t\t\t\t\t\t\t\t#{caller}\n"
+        Logger.debug "ActiveMocker :: Debug :: Can't #{type} module #{name} from class #{self.name}.\n\t\t\t\t\t\t\t\t#{_caller}\n"
         Logger.debug "\t\t\t\t\t\t\t\t#{e}"
       end
     end

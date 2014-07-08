6be22d1cd73d9f8468acf18ecc03bca8675bc569
Dustin Zeisler <dustin@zive.me>
2014-07-07 17:12:06 -0700

    To use with `before(:all)` wrap Constant in `#mock_class('ClassName')`.

--- lib/active_mocker/rspec_helper.rb
+++ lib/active_mocker/rspec_helper.rb
@@ -1,35 +1,15 @@
 RSpec.configure do |config|
 
-  def stub_class(klass)
-    if defined?(Rails) && !active_mocker?
-      return klass
-    end
-    ActiveMocker::LoadedMocks.class_name_to_mock.select { |class_name, mock| klass.name == class_name }.values.first
+  def mock_class(class_name)
+    return class_name.constantize if defined?(Rails) && !self.class.metadata[:active_mocker]
+    ActiveMocker::LoadedMocks.class_name_to_mock.select { |name, mock| name == class_name }.values.first
   end
 
-  def active_mocker?
-    bool = false
-    begin
-      bool = !!RSpec.current_example.metadata[:active_mocker]
-      return true if bool
-    rescue
-      bool = false
+  config.before(:each) do
+    if self.class.metadata[:active_mocker]
+      ActiveMocker::LoadedMocks.class_name_to_mock.each { |class_name, mock| stub_const(class_name, mock) }
     end
-
-    begin
-      bool = !!self.class.metadata[:active_mocker]
-      return true if bool
-    rescue
-      bool = false
-    end
-    bool
   end
-
-  # config.before(:each) do
-  #   if RSpec.current_example.metadata[:active_mocker]
-  #     ActiveMocker::LoadedMocks.class_name_to_mock.each { |class_name, mock| stub_const(class_name, mock) }
-  #   end
-  # end
 
   config.after(:all) do
     ActiveMocker::LoadedMocks.clear_all
---
+++ sample_app_rails_4/spec/const_stub_example_spec.rb
@@ -0,0 +1,18 @@
+require 'rspec'
+require 'active_mocker/rspec_helper'
+$:.unshift File.expand_path('../../', __FILE__)
+APP_ROOT = File.expand_path('../../', __FILE__) unless defined? APP_ROOT
+require 'lib/post_methods'
+require_relative 'mocks/micropost_mock.rb'
+require_relative 'mocks/user_mock.rb'
+
+describe MicropostMock, active_mocker:true do
+
+  before :all do
+    mock_class('Micropost').create!
+  end
+
+  it 'will create an instance of mock' do
+    expect(Micropost.first.class).to eq MicropostMock
+  end
+end

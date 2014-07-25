debb36e6a43d23e9428a8036ace6fe680a192d98
Dustin Zeisler <dustin@zive.me>
2014-07-25 16:09:15 -0700

    Replace RejectedParams and use UnknownAttributesError to follow active record

--- lib/active_mocker/mock/exceptions.rb
+++ lib/active_mocker/mock/exceptions.rb
@@ -12,13 +12,26 @@ module Mock
   class FileTypeMismatchError < StandardError
   end
 
-  class RejectedParams < Exception
+  # Raised when unknown attributes are supplied via mass assignment.
+  class UnknownAttributeError < NoMethodError
+
+    attr_reader :record, :attribute
+
+    def initialize(record, attribute)
+      @record = record
+      @attribute = attribute.to_s
+      super("unknown attribute: #{attribute}")
+    end
+
   end
 
   class Unimplemented < Exception
   end
 
   class IdNotNumber < Exception
+  end
+
+  class Error < Exception
   end
 
 end

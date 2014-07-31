6fee021e98649a124f29113af9fb2f7844fa821e
Dustin Zeisler <dustin@zive.me>
2014-07-31 07:22:57 -0700

    upgrade to rspec 3

--- test_rails_4_app/spec/spec_helper.rb
+++ test_rails_4_app/spec/spec_helper.rb
@@ -1,46 +1,18 @@
 ENV["RAILS_ENV"] ||= 'test'
-
 require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
-require 'rspec/autorun'
+Dir[Rails.root.join("spec/support/**/*.rb")].each {|f| require f}
 
-  # Requires supporting ruby files with custom matchers and macros, etc,
-  # in spec/support/ and its subdirectories.
-  Dir[Rails.root.join("spec/support/**/*.rb")].each {|f| require f}
-
-  # Checks for pending migrations before tests are run.
-  # If you are not using ActiveRecord, you can remove this line.
 ActiveRecord::Migration.maintain_test_schema!
 
 RSpec.configure do |config|
-    # ## Mock Framework
-    #
-    # If you prefer to use mocha, flexmock or RR, uncomment the appropriate line:
-    #
-    # config.mock_with :mocha
-    # config.mock_with :flexmock
-    # config.mock_with :rr
 
-    # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
+  config.use_transactional_fixtures = true
+  config.infer_base_class_for_anonymous_controllers = false
+  config.order = "random"
 
-    # If you're not using ActiveRecord, or you'd prefer not to run each of your
-    # examples within a transaction, remove the following line or assign false
-    # instead of true.
-    config.use_transactional_fixtures = true
-
-    # If true, the base class of anonymous controllers will be inferred
-    # automatically. This will be the default behavior in future versions of
-    # rspec-rails.
-    config.infer_base_class_for_anonymous_controllers = false
-
-    # Run specs in random order to surface order dependencies. If you find an
-    # order dependency and want to debug it, you can fix the order by providing
-    # the seed, which is printed after each run.
-    #     --seed 1234
-    config.order = "random"
-    # Include the Capybara DSL so that specs in spec/requests still work.
-    # Disable the old-style object.should syntax.
-    config.expect_with :rspec do |c|
-      c.syntax = :expect
-    end
+  config.expect_with :rspec do |c|
+    c.syntax = :expect
   end
+
+end

37b306e5d883c3a4c9909642eaf9bda1e6c66010
Dustin Zeisler <dustin@zeisler.net>
2014-10-24 21:43:28 -0700

    add sleep after build to pass on travis

--- spec/lib/active_mocker/generate_spec.rb
+++ spec/lib/active_mocker/generate_spec.rb
@@ -11,7 +11,7 @@ describe ActiveMocker::Generate do
   describe 'rake active_mocker:build' do
 
     before(:each) do
-      system "cd test_rails_4_app && bundle exec appraisal rake active_mocker:build"
+      `cd test_rails_4_app && bundle exec appraisal rake active_mocker:build`
     end
 
     it 'generates all mocks files' do
@@ -31,13 +31,14 @@ describe ActiveMocker::Generate do
       FileUtils.rm(File.join(mock_dir, 'micropost_mock.rb'))
       expect(File.exist?(File.join(mock_dir, 'user_mock.rb'))).to eq false
       expect(File.exist?(File.join(mock_dir, 'micropost_mock.rb'))).to eq false
-      system "cd test_rails_4_app && bundle exec appraisal rake active_mocker:build MODEL=user"
+      `cd test_rails_4_app && bundle exec appraisal rake active_mocker:build MODEL=user`
+      sleep 2 # add sleep to get to pass on travis
       expect(File.exist? File.join(mock_dir, 'user_mock.rb')).to eq true
       expect(File.exist?(File.join(mock_dir, 'micropost_mock.rb'))).to eq false
     end
 
     after do
-      system "cd test_rails_4_app && bundle exec appraisal rake active_mocker:build MODEL=Micropost"
+      `cd test_rails_4_app && bundle exec appraisal rake active_mocker:build MODEL=Micropost`
     end
 
   end

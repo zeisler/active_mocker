8ad9806c1374d80422baf8aa1629b1f338b98d38
Dustin Zeisler <dustin@zeisler.net>
2014-12-06 16:24:40 -0800

    simplify code

--- lib/active_mocker/mock/collection.rb
+++ lib/active_mocker/mock/collection.rb
@@ -6,6 +6,10 @@ module Mock
   class Collection
 
     include Enumerable
+    extend ::Forwardable
+    def_delegators :@collection, :[], :take, :push, :clear, :first, :last, :concat, :replace, :uniq, :count, :size, :length, :empty?, :any?, :many?, :include?, :delete
+    alias_method :distinct, :uniq
+
 
     def initialize(collection=[])
       @collection = [*collection]
@@ -15,23 +19,9 @@ module Mock
       collection.concat(records.flatten)
     end
 
-    extend ::Forwardable
-    def_delegators :@collection, :[], :take, :push, :clear, :first, :last, :concat, :replace, :uniq, :count, :size, :length, :empty?, :any?, :many?, :include?, :delete
-    alias_method :distinct, :uniq
-
-    def select(&block)
-      collection.select(&block)
-    end
-
     def each(&block)
       collection.each do |item|
-        block.call(item)
-      end
-    end
-
-    def map(&block)
-      collection.map do |item|
-        block.call(item)
+        yield(item)
       end
     end
 
@@ -51,7 +41,6 @@ module Mock
       @collection == val
     end
 
-    # Returns true if relation is blank.
     def blank?
       to_a.blank?
     end

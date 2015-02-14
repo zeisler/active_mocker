a9bd2ca8906a152ce7aee0ae084812ee57c7ba80
Dustin Zeisler <dustin@zeisler.net>
2015-02-13 19:58:30 -0800

    Remove extra object allocation and refactor internals of Records class.

--- lib/active_mocker/mock/records.rb
+++ lib/active_mocker/mock/records.rb
@@ -1,62 +1,62 @@
 module ActiveMocker
-module Mock
-class Records
+  module Mock
+    class Records
 
-  extend Forwardable
-  def_delegators :records, :<<, :count, :length, :to_a
+      extend Forwardable
+      def_delegators :records, :<<, :count, :length, :to_a
 
-  attr_reader :records, :record_index
-  private     :records, :record_index
-  def initialize(records = [])
-    @records      = records
-    @record_index = {}
+      attr_reader :records
+      private     :records
+
+      def initialize(records = [])
+        @records = records
+      end
+
+      def insert(record)
+        records << validate_unique_id((record.id ||= next_id), record)
+      end
+
+      def delete(record)
+        raise RecordNotFound, 'Record has not been created.' unless records.delete(record)
+      end
+
+      def exists?(record)
+        records.include?(record)
+      end
+
+      def new_record?(record)
+        !exists?(record)
+      end
+
+      def persisted?(id)
+        ids.include?(id)
+      end
+
+      def reset
+        records.clear
+      end
+
+      private
+
+      def ids
+        records.map(&:id)
+      end
+
+      def next_id
+        max_record.succ
+      rescue NoMethodError
+        1
+      end
+
+      def max_record
+        ids.max
+      end
+
+      def validate_unique_id(id, record)
+        raise IdError, "Duplicate ID found for record #{record.inspect}" if persisted?(id)
+        record
+      end
+
+    end
   end
-
-  def insert(record)
-    record.attributes[:id] ||= next_id
-    validate_unique_id(record)
-    add_to_record_index(record)
-    records << record
-  end
-
-  def delete(record)
-    raise RecordNotFound, 'Record has not been created.' if new_record?(record)
-    record_index.delete("#{record.id}")
-    index = records.index(record)
-    records.delete_at(index)
-  end
-
-  def exists?(record)
-    records.include?(record)
-  end
-
-  def new_record?(record)
-    !exists?(record)
-  end
-
-  def persisted?(id)
-    records.map(&:id).include?(id)
-  end
-
-  def reset
-    record_index.clear
-    records.clear
-  end
-
-  private
-
-  def next_id
-    NextId.new(records).next
-  end
-
-  def add_to_record_index(record)
-    record_index.merge!({record.id.to_s => records.length})
-  end
-
-  def validate_unique_id(record)
-    raise IdError, "Duplicate ID found for record #{record.inspect}" if record_index.has_key?(record.id.to_s)
-  end
-
-end
-end
 end
--- spec/lib/active_mocker/mock/records_spec.rb
+++ spec/lib/active_mocker/mock/records_spec.rb
@@ -1,58 +1,48 @@
 require 'spec_helper'
 require 'active_mocker/mock/records'
-require 'active_mocker/mock/next_id'
 require 'active_mocker/mock/exceptions'
 require 'active_support/hash_with_indifferent_access'
+require 'active_support/core_ext/object/try'
 
 describe ActiveMocker::Mock::Records do
 
-  subject{described_class.new}
+  subject { described_class.new }
 
-  let(:record){ RecordBase.new}
+  let(:record) { RecordBase.new }
 
   before do
     class RecordBase
 
-      def attributes
-        @attributes ||= HashWithIndifferentAccess.new
-      end
-
-      def id
-        attributes['id']
-      end
+      attr_accessor :id
 
       def inspect
-        {id: id}
+        { id: id }
       end
 
     end
   end
 
-    describe '#insert' do
+  describe '#insert' do
 
-      before do
-        subject.insert(record)
-      end
-
-      it 'adds to records' do
-        expect(subject.to_a).to include(record)
-      end
-
-      it 'gets next id' do
-        expect(record.id).to eq 1
-      end
-
-      it 'adds to record index' do
-        expect(subject.send(:record_index)).to eq({ '1' => 0 })
-      end
-
-      it 'validate unique id' do
-        new_record = RecordBase.new
-        new_record.attributes[:id] = 1
-        expect{subject.insert(new_record)}.to raise_exception(ActiveMocker::Mock::IdError, 'Duplicate ID found for record {:id=>1}')
-      end
-
+    before do
+      subject.insert(record)
     end
+
+    it 'adds to records' do
+      expect(subject.to_a).to include(record)
+    end
+
+    it 'gets next id' do
+      expect(record.id).to eq 1
+    end
+
+    it 'validate unique id' do
+      new_record    = RecordBase.new
+      new_record.id = 1
+      expect { subject.insert(new_record) }.to raise_exception(ActiveMocker::Mock::IdError, 'Duplicate ID found for record {:id=>1}')
+    end
+
+  end
 
   describe '#delete' do
 
@@ -61,17 +51,12 @@ describe ActiveMocker::Mock::Records do
       subject.delete(record)
     end
 
-    it 'removes from the record index' do
-      expect(subject.send(:record_index)).to eq({})
-    end
-
     it 'deletes from record array' do
       expect(subject.to_a).to eq []
     end
 
     it 'raises if record is not in array' do
-      subject = described_class.new
-      expect{subject.delete(record)}.to raise_error(ActiveMocker::Mock::RecordNotFound, 'Record has not been created.')
+      expect { described_class.new.delete(record) }.to raise_error(ActiveMocker::Mock::RecordNotFound, 'Record has not been created.')
     end
 
   end
@@ -120,7 +105,6 @@ describe ActiveMocker::Mock::Records do
     it 'clears records array and record_index hash' do
       subject.insert(record)
       subject.reset
-      expect(subject.send(:record_index)).to eq({})
       expect(subject.send(:records)).to eq([])
     end
 

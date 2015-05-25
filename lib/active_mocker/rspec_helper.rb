113170a6feafb6956594d902e3ca45f412a8163f
Dustin Zeisler <dustin@zeisler.net>
2015-05-24 17:37:07 -0700

    move rspec module out of rspec_helper

--- lib/active_mocker/rspec_helper.rb
+++ lib/active_mocker/rspec_helper.rb
@@ -1,61 +1,10 @@
 require 'active_mocker/loaded_mocks'
+require 'active_mocker/rspec'
 
-module ActiveMocker
-  module RSpec
-
-    class Interface
-
-      def initialize(parent)
-        @parent   = parent
-        @metadata = parent.class.metadata
-      end
-
-      def mocks
-        _am_mocks_with_scope
-      end
-
-      private
-      attr_reader :parent, :metadata
-
-      def _am_mocks_with_scope
-        ActiveMocker::LoadedMocks.scoped_set(_am_uniq_key_for_example)
-      end
-
-      def _am_class_name_to_mock
-        ActiveMocker::LoadedMocks.scoped_set(_am_uniq_key_for_example)
-      end
-
-      def _am_uniq_key_for_example
-        Digest::MD5.hexdigest _am_outermost_parent_example[:location] + _am_outermost_parent_example[:full_description]
-      end
-
-      def _am_outermost_parent_example(group: metadata[:parent_example_group] || metadata)
-        if group[:parent_example_group].nil?
-          group
-        else
-          _am_outermost_parent_example(group: group[:parent_example_group])
-        end
-      end
-    end
-
-    def active_mocker
-      Interface.new(self)
-    end
-
-    # Deprecated method, will be removed in version 2.1
-    # Use +active_mocker.mocks.find('ClassName')+ instead
-    def mock_class(*args)
-      active_mocker.mocks.find(*args)
-    end
-
-    private
-
-  end
-end
 
 RSpec.configure do |config|
 
-  config.include ActiveMocker::RSpec
+  config.include ActiveMocker::Rspec
 
   config.before(:each, active_mocker: true) do
     unless ENV['RUN_WITH_RAILS'] && self.class.metadata[:rails_compatible]
@@ -63,7 +12,7 @@ RSpec.configure do |config|
     end
   end
 
-  config.after(:all,active_mocker: true) do
+  config.after(:all, active_mocker: true) do
     if ActiveMocker::LoadedMocks.disable_global_state
       ActiveMocker::LoadedMocks.deallocate_scoped_set(active_mocker.send(:_am_uniq_key_for_example))
     else

0772ab45d4e95bb570da018d0651c59108d4603e
Dustin Zeisler <dustin@zeisler.net>
2015-12-23 19:05:31 -0800

    Add Test to make sure test gem files have current gem version to save travis from failing.

---
+++ spec/gemfile_version_match_spec.rb
@@ -0,0 +1,16 @@
+RSpec.describe "Gemfile Version Match" do
+  it "rails_4.0.gemfile.lock" do
+    file = File.join(File.expand_path('../../', __FILE__), "test_rails_4_app/gemfiles/rails_4.0.gemfile.lock")
+    expect(File.readlines(file).grep(/active_mocker \(#{Regexp.quote(ActiveMocker::VERSION)}\)/).size > 0).to eq true
+  end
+
+  it "rails_4.1.gemfile.lock" do
+    file = File.join(File.expand_path('../../', __FILE__), "test_rails_4_app/gemfiles/rails_4.1.gemfile.lock")
+    expect(File.readlines(file).grep(/active_mocker \(#{Regexp.quote(ActiveMocker::VERSION)}\)/).size > 0).to eq true
+  end
+
+  it "rails_4.2.gemfile.lock" do
+    file = File.join(File.expand_path('../../', __FILE__), "test_rails_4_app/gemfiles/rails_4.2.gemfile.lock")
+    expect(File.readlines(file).grep(/active_mocker \(#{Regexp.quote(ActiveMocker::VERSION)}\)/).size > 0).to eq true
+  end
+end

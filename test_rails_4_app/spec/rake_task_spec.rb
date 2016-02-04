1f805d90513bf79eee97e27f9f4b67b8fb165882
Dustin Zeisler <dustin@zeisler.net>
2016-02-04 06:57:32 -0800

    Diagnositic for travis failure.

--- test_rails_4_app/spec/rake_task_spec.rb
+++ test_rails_4_app/spec/rake_task_spec.rb
@@ -1,14 +1,18 @@
 require 'rails_helper'
-require 'rake'
 
 describe "rake active_mocker:build" do
-  before { SampleApp::Application.load_tasks }
   it "builds the mocks from the model dir" do
-    expect(File.exist? mock_path                                   ).to eq true
-    expect(File.exist? File.join(mock_path, 'user_mock.rb')        ).to eq true
-    expect(File.exist? File.join(mock_path, 'micropost_mock.rb')   ).to eq true
+    expect(File.exist? mock_path).to eq true
+    expect(File.exist? File.join(mock_path, 'user_mock.rb')).to eq true
+    expect(File.exist? File.join(mock_path, 'micropost_mock.rb')).to eq true
     expect(File.exist? File.join(mock_path, 'relationship_mock.rb')).to eq true
-    expect(File.exist? File.join(mock_path, 'api/customer_mock.rb')).to eq true
+    result = File.exist?(File.join(mock_path, 'api/customer_mock.rb'))
+    puts ""
+    puts mock_path
+    system "ls #{mock_path}"
+    puts File.join(mock_path, 'api')
+    system "ls #{File.join(mock_path, 'api')}"
+    expect(result).to eq true
     expect(File.exist? File.join(mock_path, 'nacis_mock.rb')).to eq false
   end
 end

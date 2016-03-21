f0fc4acad79b506b8f62b98df84f69aa7a574435
Dustin Zeisler <dustin@zeisler.net>
2016-03-21 15:24:15 -0700

    Fix - Issue where namespaced mocks would not be auto stubbed.

---
+++ test_rails_4_app/spec/api/customer_spec.rb
@@ -0,0 +1,10 @@
+require 'rails_helper'
+require_mock 'api/customer_mock'
+
+RSpec.describe Api::CustomerMock do
+  describe "::mocked_class" do
+    it "returns the class name with the namespace" do
+      expect(described_class.send(:mocked_class)).to eq "Api::Customer"
+    end
+  end
+end

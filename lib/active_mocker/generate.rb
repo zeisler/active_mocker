011979cf69a5cf7305b1647a8e721677c32d6b44
Mark Burns <markthedeveloper@gmail.com>
2016-04-28 10:46:19 +0900

    extract class refactor

--- lib/active_mocker/generate.rb
+++ lib/active_mocker/generate.rb
@@ -14,21 +14,8 @@ module ActiveMocker
       progress_init
 
       active_record_models_with_files.each do |model, file|
-        model_name      = model.name
-
-        mock_file_name  = "#{model_name.underscore}_#{config.mock_append_name.underscore}.rb"
-        mock_file_path  = File.join(Config.mock_dir, mock_file_name)
-        assure_dir_path_exists(mock_file_path)
-        schema_scrapper = ActiveRecordSchemaScrapper.new(model: model)
-        File.open(mock_file_path, "w") do |file_out|
-          begin
-            result                       = create_mock(file, file_out, schema_scrapper)
-            status                       = collect_errors(mock_file_path, result.errors, schema_scrapper, model_name)
-            display_errors.success_count += 1 if result.completed? && status.successful?
-          rescue => e
-            rescue_clean_up(e, file_out, model_name)
-          end
-        end
+        writer = FileWriter.new(model: model, file: file, display_errors: display_errors, config: config, model_names: model_names)
+        writer.write!
 
         progress.increment
       end
@@ -54,38 +41,6 @@ module ActiveMocker
 
     attr_reader :display_errors, :progress
 
-    def create_mock(file, file_out, schema_scrapper)
-      MockCreator.new(file:                 File.open(file),
-                      file_out:             file_out,
-                      schema_scrapper:      schema_scrapper,
-                      klasses_to_be_mocked: model_names,
-                      enabled_partials:     enabled_partials,
-                      mock_append_name:     config.mock_append_name).create
-    end
-
-    OtherErrors = Struct.new(:successful?)
-
-    def collect_errors(mock_file_path, create_mock_errors, schema_scrapper, model_name)
-      display_errors.wrap_errors(schema_scrapper.associations.errors, model_name, type: :associations)
-      display_errors.wrap_errors(schema_scrapper.attributes.errors, model_name, type: :attributes)
-
-      if create_mock_errors.present? || schema_scrapper.attributes.errors.any? { |e| e.level == :error }
-        display_errors.failed_models << model_name
-        File.delete(mock_file_path) if File.exist?(mock_file_path)
-        display_errors.add(create_mock_errors)
-        OtherErrors.new(false)
-      else
-        OtherErrors.new(true)
-      end
-    end
-
-    def rescue_clean_up(e, file_out, model_name)
-      display_errors.failed_models << model_name
-      file_out.close unless file_out.closed?
-      File.delete(file_out.path) if File.exist?(file_out.path)
-      display_errors.wrap_an_exception(e, model_name)
-    end
-
     def progress_init
       @progress = config.progress_class.create(active_record_models.count)
     end
@@ -109,12 +64,6 @@ module ActiveMocker
       FilePathToRubyClass.new(base_path: config.model_dir, class_path: file).to_s
     end
 
-    def assure_dir_path_exists(file)
-      unless File.exist?(File.dirname(file))
-        FileUtils.mkdir_p(File.dirname(file))
-      end
-    end
-
     def config
       ActiveMocker::Config
     end
@@ -123,20 +72,103 @@ module ActiveMocker
       FileUtils.mkdir_p(config.mock_dir) unless Dir.exist?(config.mock_dir)
     end
 
-    def enabled_partials
-      if config.disable_modules_and_constants
-        MockCreator.enabled_partials_default - [*:modules_constants]
-      else
-        MockCreator.enabled_partials_default
-      end
-    end
-
     def clean_up
       delete_mocks unless config.single_model_path
     end
 
     def delete_mocks
       FileUtils.rm Dir.glob(File.join(config.mock_dir, "/**/*_#{config.mock_append_name.underscore}.rb"))
+    end
+
+    class FileWriter
+      include Virtus.model
+      delegate :name, to: :model, prefix: true
+
+      attribute :model, Object
+      attribute :file, String
+      attribute :display_errors
+      attribute :config
+      attribute :model_names, Array
+
+      def write!
+        assure_dir_path_exists!
+
+        safe_write do |file_out|
+          result = create_mock(file_out)
+          status = collect_errors(result.errors)
+          display_errors.success_count += 1 if result.completed? && status.successful?
+        end
+      end
+
+      private
+
+      def safe_write
+        File.open(mock_file_path, 'w') do |file_out|
+          begin
+            yield file_out
+          rescue StandardError => e
+            rescue_clean_up(e, file_out)
+          end
+        end
+      end
+
+      def rescue_clean_up(e, file_out)
+        display_errors.failed_models << model_name
+        file_out.close unless file_out.closed?
+        File.delete(file_out.path) if File.exists?(file_out.path)
+        display_errors.wrap_an_exception(e, model_name)
+      end
+
+      def schema_scrapper
+        @schema_scrapper ||= ActiveRecordSchemaScrapper.new(model: model)
+      end
+
+      def mock_file_path
+        File.join(Config.mock_dir, mock_file_name)
+      end
+
+      def mock_file_name
+        "#{model_name.underscore}_#{config.mock_append_name.underscore}.rb"
+      end
+
+      def assure_dir_path_exists!
+        unless File.exists?(File.dirname(mock_file_path))
+          FileUtils::mkdir_p(File.dirname(mock_file_path))
+        end
+      end
+
+      def create_mock(file_out)
+        MockCreator.new(file:                 File.open(file),
+                        file_out:             file_out,
+                        schema_scrapper:      schema_scrapper,
+                        klasses_to_be_mocked: model_names,
+                        enabled_partials:     enabled_partials,
+                        mock_append_name:     config.mock_append_name).create
+      end
+
+      OtherErrors = Struct.new(:successful?)
+
+      def collect_errors(create_mock_errors)
+        display_errors.wrap_errors(schema_scrapper.associations.errors, model_name, type: :associations)
+        display_errors.wrap_errors(schema_scrapper.attributes.errors,   model_name, type: :attributes)
+
+        if create_mock_errors.present? || schema_scrapper.attributes.errors.any? { |e| e.level == :error }
+          display_errors.failed_models << model_name
+          File.delete(mock_file_path) if File.exists?(mock_file_path)
+          display_errors.add(create_mock_errors)
+          OtherErrors.new(false)
+        else
+          OtherErrors.new(true)
+        end
+      end
+
+      def enabled_partials
+        if config.disable_modules_and_constants
+          MockCreator.enabled_partials_default - [*:modules_constants]
+        else
+          MockCreator.enabled_partials_default
+        end
+      end
     end
   end
 end

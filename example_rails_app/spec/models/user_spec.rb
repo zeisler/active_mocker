f8cccd751eba3eb7d0d98d675ce34f9e448234f2
Mark Burns <markthedeveloper@gmail.com>
2016-05-02 11:19:25 +0900

    fix hound dog suggestions, ignore example app mocks

--- example_rails_app/spec/models/user_spec.rb
+++ example_rails_app/spec/models/user_spec.rb
@@ -1,3 +1,4 @@
+# frozen_string_literal: true
 require "rails_helper"
 require "spec/mocks/user_mock"
 
@@ -11,13 +12,13 @@ RSpec.describe User, no_db: true do
 
   context "with active mocker in use", active_mocker: true do
     it "doesn't touch the db with the nosql gem in use" do
-      expect{code.call}.not_to raise_error Nosql::Error
+      expect { code.call }.not_to raise_error Nosql::Error
     end
   end
 
   context "with active mocker in use", active_mocker: false do
     it "touches the db" do
-      expect{code.call}.to raise_error Nosql::Error, ["PRAGMA table_info(\"users\")", "SCHEMA"].inspect
+      expect { code.call }.to raise_error Nosql::Error, ["PRAGMA table_info(\"users\")", "SCHEMA"].inspect
     end
   end
 end

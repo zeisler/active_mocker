72bca29e681a2b6f526b2d1f5e28959d0b54c255
Dustin Zeisler <dustin@zeisler.net>
2017-03-17 13:26:54 -0700

    v.2.5.0

--- lib/active_mocker/version.rb
+++ lib/active_mocker/version.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 module ActiveMocker
-  VERSION = "2.4.4"
+  VERSION = "2.5.0"
   module Mock
     VERSION = "1" # This increments when breaking changes happen in the generated mocks
   end

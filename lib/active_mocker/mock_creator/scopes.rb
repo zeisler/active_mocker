e8f022768ed2c5c0cea8cb5f5cdef714c2a18c89
Dustin Zeisler <dustin@zeisler.net>
2017-03-17 13:30:08 -0700

    Fix style

--- lib/active_mocker/mock_creator/scopes.rb
+++ lib/active_mocker/mock_creator/scopes.rb
@@ -10,22 +10,32 @@ module ActiveMocker
         class_introspector.class_macros.select { |h| h.keys.first == :scope }.map do |h|
           name, args = h.values.first.first
           arguments  = ReverseParameters.new(args, blocks_as_values: true)
-
-          body       = if safe_methods[:scopes].include?(name)
-                         find_scope_body(name)
-                       else
-                         "#{class_name}.send(:call_mock_method, method: '#{name}', caller: Kernel.caller, arguments: [#{arguments.arguments}])"
-                       end
+          body       = scope_body(arguments, name)
           Method.new(name, arguments, body)
         end
       end
 
-      def ast_scopes
-        @ast_scopes ||= class_introspector.parsed_source.class_begin.children.select { |n| n.try(:type) == :send && n.try{children[1] == :scope} }
+      def scope_body(arguments, name)
+        if safe_methods[:scopes].include?(name)
+          find_scope_body_from_ast(name)
+        else
+          "#{class_name}.send(:call_mock_method, " \
+                           "method: '#{name}', " \
+                           "caller: Kernel.caller, " \
+                           "arguments: [#{arguments.arguments}])"
+        end
       end
 
-      def find_scope_body(name)
-        scope = ast_scopes.detect {|n| n.children[2].children.first == name }
+      def ast_scopes
+        @ast_scopes ||= class_introspector
+                          .parsed_source
+                          .class_begin
+                          .children
+                          .select { |n| n.try(:type) == :send && n.try { children[1] == :scope } }
+      end
+
+      def find_scope_body_from_ast(name)
+        scope = ast_scopes.detect { |n| n.children[2].children.first == name }
         DissociatedIntrospection::RubyCode.build_from_ast(scope.children[3].children[2]).source
       end
     end

3a35a75c9a2da22983de903e4c20f82c9f63c8c9
Dustin Zeisler <dustin@zeisler.net>
2017-11-17 09:09:59 -0800

    Error message when calling stubbed methods show safe_methods syntax along with RSpec syntax.

--- lib/active_mocker/mock/mockable_method.rb
+++ lib/active_mocker/mock/mockable_method.rb
@@ -5,13 +5,15 @@ module ActiveMocker
     private
 
     def __am_raise_not_mocked_error(method:, caller:, type:)
-      variable_name = if is_a?(Relation)
-                        "#{self.class.name.underscore.split("/").first}_relation"
-                      elsif type == "#"
-                        "#{self.class.name.underscore}_record"
-                      else
-                        name
-                      end
+      variable_name, klass, method_type = if is_a?(Relation)
+                                            klass = self.class.name.underscore.split("/").first
+                                            ["#{klass}_relation", klass.camelize, :scopes]
+                                          elsif type == "#"
+                                            klass = self.class.name
+                                            ["#{klass.underscore}_record", klass, :instance_methods]
+                                          else
+                                            [name, name, :class_methods]
+                                          end
 
       message = <<-ERROR.strip_heredoc
         Unknown implementation for mock method: #{variable_name}.#{method}
@@ -21,6 +23,11 @@ module ActiveMocker
           allow(
             #{variable_name}
           ).to receive(:#{method}).and_return(:some_expected_result)
+
+        OR Whitelist the method as safe to copy/run in the context of ActiveMocker (requires mock rebuild)
+        # ActiveMocker.safe_methods #{method_type}: [:#{method}]
+        class #{klass.gsub("Mock", "")} < ActiveRecord::Base
+        end
       ERROR
 
       raise NotImplementedError, message, caller

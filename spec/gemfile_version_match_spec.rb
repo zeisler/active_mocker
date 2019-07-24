3bf7162cb71e3e94b7b528d565685cd4110d2231
Dustin Zeisler <dustin@zeisler.net>
2019-07-24 10:37:37 -0700

    Testing for rails 6

--- spec/gemfile_version_match_spec.rb
+++ spec/gemfile_version_match_spec.rb
@@ -5,13 +5,13 @@ RSpec.describe "Gemfile Version Match" do
     expect(!File.readlines(file).grep(/active_mocker \(#{Regexp.quote(ActiveMocker::VERSION)}\)/).empty?).to eq true
   end
 
-  it "rails_5.1.gemfile.lock" do
+  it "rails_5.2.gemfile.lock" do
     file = File.join(File.expand_path("../../", __FILE__), "test_rails_app/gemfiles/rails_5.1.gemfile.lock")
     expect(!File.readlines(file).grep(/active_mocker \(#{Regexp.quote(ActiveMocker::VERSION)}\)/).empty?).to eq true
   end
 
-  it "rails_5.2.gemfile.lock" do
-    file = File.join(File.expand_path("../../", __FILE__), "test_rails_app/gemfiles/rails_5.2.gemfile.lock")
+  it "rails_6.0.gemfile.lock" do
+    file = File.join(File.expand_path("../../", __FILE__), "test_rails_app/gemfiles/rails_6.0.gemfile.lock")
     expect(!File.readlines(file).grep(/active_mocker \(#{Regexp.quote(ActiveMocker::VERSION)}\)/).empty?).to eq true
   end
 end
